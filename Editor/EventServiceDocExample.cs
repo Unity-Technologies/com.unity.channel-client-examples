a034a4be8242327986ca1769b6c59346c0cd2166
Sebastien Phaneuf <lochrist@yahoo.com>
2020-03-17 17:20:34 -0400

    Add ChannelComm Doc example

---
+++ Editor/EventServiceDocExample.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class EventServiceDocExample
+{
+    // Setup listeners
+
+    // Emit message
+
+    // Request
+}
